67f36f8580cd8425cb6fe95811dbeb5d344ef276
SKRJS <zubidev71@gmail.com>
2025-01-06 23:54:22 +0600

    ok

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>counter with redux</h1>
+      <button>increment</button>
+       <div className="div">0</div>
+      <button>decrement</button>
+    </>
+  )
+}
+
+export default App
+ 
