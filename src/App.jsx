90758572a24efcc028ef19b53a5c68cd5e15d31a
SKRJS <zubidev71@gmail.com>
2025-01-14 23:41:43 +0600

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { useDispatch, useSelector } from "react-redux";
+import { add, remove } from "./redux/features/counter/counterSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const buttonAdd = () => {
+    dispatch(add());
+  }
+  const buttonRemove = () => {
+    dispatch(remove());
+  }
+
+  const {count} = useSelector((state) => state.counter)
+
   return (
     <>
       <h1>counter with redux</h1>
-      <button>increment</button>
-       <div className="div">0</div>
-      <button>decrement</button>
+      <button onClick={buttonAdd} >increment</button>
+       <div className="div">{count}</div>
+      <button onClick={buttonRemove}>decrement</button>
     </>
   )
 }
--- src/redux/features/counter/counterSlice.ts
+++ src/redux/features/counter/counterSlice.ts
@@ -6,7 +6,15 @@ const initialState = {
 const counterSlice = createSlice({
   name: "counter",
   initialState,
-  reducers: {},
+  reducers: {
+    add: (state) => {
+      state.count = state.count + 1;
+    },
+    remove: (state) => {
+      state.count = state.count - 1;
+    },
+  },
 });
 
+export const { add, remove } = counterSlice.actions;
 export default counterSlice.reducer;
